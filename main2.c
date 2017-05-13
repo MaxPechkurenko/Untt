ef6382b939f20cc27b2b28a1134fac85ae0ae227
maxmxmx <max.pechkurenko@gmail.com>
2017-05-13 04:43:03 +0300

    Add main2.c

---
+++ main2.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+int age, days;
+int count = 0;
+printf("Enter your age: "); //comment
+scanf("%d", &age);
+if(age < 0)
+{
+printf("Wrong age!! Enter correct age: ");
+scanf("%d", &age);
+}
+for(int i = 2017 - age; i <= 2017; i++)
+{
+if(i%4 == 0){
+count++;
+}
+}
+days = 364 * count + 365 * (age - count);
+printf("You are on the Earth ");
+printf("%d", days);
+printf(" days\n");
+printf("end");
+}
