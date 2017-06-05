abed73035542fa942027f426bc708d0689c7928c
maxmxmx <max.pechkurenko@gmail.com>
2017-06-05 16:32:45 +0300

    add lab8_2(signin/signup)

---
+++ lab_tp_8/Lab8_2/Lab8_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab8_2
+//
+//  Created by Admin on 05.6.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lab_tp_8/Lab8_2/Lab8_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab8_2
+//
+//  Created by Admin on 05.6.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
